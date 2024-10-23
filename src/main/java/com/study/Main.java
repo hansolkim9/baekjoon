1a2c34f2701851973e0c22096b500d9e59ebd790
hansolkim9 <masicer2@gmail.com>
2024-10-23 18:28:01 +0900

    2164

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -1,11 +1,26 @@
 package com.study;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        int N = scanner.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
         }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.poll());
     }
 }
