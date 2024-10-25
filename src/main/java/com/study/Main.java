91243c3ee1cb0ab349395e51936840cf359e9862
hansolkim9 <masicer2@gmail.com>
2024-10-25 14:30:00 +0900

    11723

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -1,27 +1,47 @@
 package com.study;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        int S = 0;  // 비트마스크를 위한 정수 초기화
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
-            int a = scanner.nextInt();
-            if (a != 0) {
-                stack.push(a);
-            } else {
-                stack.pop();
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            int x;
+
+            switch (command) {
+                case "add":
+                    x = Integer.parseInt(st.nextToken());
+                    S |= (1 << x);  // x번째 비트를 1로 설정
+                    break;
+                case "remove":
+                    x = Integer.parseInt(st.nextToken());
+                    S &= ~(1 << x);  // x번째 비트를 0으로 설정
+                    break;
+                case "check":
+                    x = Integer.parseInt(st.nextToken());
+                    sb.append((S & (1 << x)) != 0 ? 1 : 0).append("\n");
+                    break;
+                case "toggle":
+                    x = Integer.parseInt(st.nextToken());
+                    S ^= (1 << x);  // x번째 비트를 반전
+                    break;
+                case "all":
+                    S = (1 << 21) - 1;  // 1부터 20까지 모두 1로 설정
+                    break;
+                case "empty":
+                    S = 0;  // 집합을 비움
+                    break;
             }
         }
-
-        int sum = 0;
-        for (int num : stack) {
-            sum += num;
-        }
-        System.out.println(sum);
+        System.out.print(sb);
     }
 }
