5f65aa57d6a4aed0f31e3129afadbd98fb7ff27e
hansolkim9 <masicer2@gmail.com>
2024-10-27 22:07:46 +0900

    18110

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -1,33 +1,29 @@
 package com.study;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
+        int n = Integer.parseInt(reader.readLine());
+        int k = (int) Math.round(n * 0.15);
+        int[] arr = new int[n];
 
-        Queue<Integer> q = new LinkedList<>();
-        StringBuilder result = new StringBuilder("<");
-
-        for (int i = 1; i <= N; i++) {
-            q.add(i);
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(reader.readLine());
         }
 
-        while (!q.isEmpty()) {
-            for (int i = 1; i < K; i++) {
-                q.add(q.poll());
-            }
-            result.append(q.poll());
-            if (!q.isEmpty()) {
-                result.append(", ");
-            }
+        Arrays.sort(arr);
+        int sum = 0;
+        for (int i = k; i < n - k; i++) {
+            sum += arr[i];
         }
-        result.append(">");
-        System.out.println(result);
+
+        int m = n - 2 * k;
+        System.out.println((int) Math.round((double) sum / m));
     }
 }
