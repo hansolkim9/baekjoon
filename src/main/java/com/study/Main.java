2e5827d26f46792375318c9fee6808fe1da4d6c4
hansolkim9 <masicer2@gmail.com>
2024-10-27 14:06:47 +0900

    10845

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -1,34 +1,40 @@
 package com.study;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> q = new LinkedList<>();
 
         int N = scanner.nextInt();
+        int last = -1;
 
         for (int i = 0; i < N; i++) {
             String s = scanner.next();
 
             switch (s) {
                 case "push":
-                    stack.push(scanner.nextInt());
+                    last = scanner.nextInt();
+                    q.add(last);
                     break;
                 case "pop":
-                    System.out.println(stack.isEmpty() ? -1 : stack.pop());
+                    System.out.println(q.isEmpty() ? -1 : q.poll());
                     break;
                 case "size":
-                    System.out.println(stack.size());
+                    System.out.println(q.size());
                     break;
                 case "empty":
-                    System.out.println(stack.isEmpty() ? 1 : 0);
+                    System.out.println(q.isEmpty() ? 1 : 0);
                     break;
-                case "top":
-                    System.out.println(stack.isEmpty() ? -1 : stack.peek());
+                case "front":
+                    System.out.println(q.isEmpty() ? -1 : q.peek());
+                    break;
+                case "back":
+                    System.out.println(q.isEmpty() ? -1 : last);
                     break;
             }
         }
