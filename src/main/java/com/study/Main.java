15ec41c596cb2b12cbbaf13f96a81634319f2c1a
hansolkim9 <masicer2@gmail.com>
2024-10-30 16:44:16 +0900

    17874

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
+        int n = scanner.nextInt();
+        int h = scanner.nextInt();
+        int v = scanner.nextInt();
 
-        int seconds = a+b+c+d;
-        System.out.println(seconds/60);
-        System.out.println(seconds%60);
+        int x = Math.max(h, n-h);
+        int y = Math.max(v, n-v);
+
+        System.out.println(x*y*4);
     }
 }
