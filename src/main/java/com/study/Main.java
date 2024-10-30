06507c104ce916b3d5a0592e5d2fb48810b734e3
hansolkim9 <masicer2@gmail.com>
2024-10-30 16:10:37 +0900

    25314

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -1,29 +1,15 @@
 package com.study;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(reader.readLine());
-        int k = (int) Math.round(n * 0.15);
-        int[] arr = new int[n];
+        int N = scanner.nextInt();
+        int n = N/4;
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(reader.readLine());
-        }
-
-        Arrays.sort(arr);
-        int sum = 0;
-        for (int i = k; i < n - k; i++) {
-            sum += arr[i];
-        }
-
-        int m = n - 2 * k;
-        System.out.println((int) Math.round((double) sum / m));
+        System.out.println("long ".repeat(n) + "int");
     }
 }
