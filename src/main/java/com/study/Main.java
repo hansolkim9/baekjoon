c2564352b95ecf8ff74d731f860e3ec2bd9bcb27
hansolkim9 <masicer2@gmail.com>
2024-10-31 17:54:38 +0900

    1764

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -6,31 +6,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int N = scanner.nextInt(); // 듣도 못한 사람
-        int M = scanner.nextInt(); // 보도 못한 사람
-        Map<String, Integer> map = new HashMap<>();
+        int N = scanner.nextInt(); // 듣도 못한 사람 수
+        int M = scanner.nextInt(); // 보도 못한 사람 수
 
+        Set<String> unheard = new HashSet<>();
+        Set<String> unseen = new HashSet<>();
+
+        // 듣도 못한 사람을 Set에 추가
         for (int i = 0; i < N; i++) {
-            map.put(scanner.next(), 0);
+            unheard.add(scanner.next());
         }
 
+        // 보도 못한 사람을 Set에 추가
         for (int j = 0; j < M; j++) {
             String name = scanner.next();
-            if (map.containsKey(name)) { // 듣도 못한 사람 중 보도 못한 사람만 카운트 증가
-                map.put(name, 1); // 값을 1로 설정해 듣도 보도 못한 사람 표시
+            if (unheard.contains(name)) {
+                unseen.add(name);  // 듣도 못한 사람에 포함된 경우에만 unseen에 추가
             }
         }
 
-        // 값이 1인 키를 list에 추가
-        List<String> resultList = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == 1) {
-                resultList.add(entry.getKey());
-            }
-        }
-
-        // 리스트 정렬 및 출력
+        // 결과 리스트 정렬
+        List<String> resultList = new ArrayList<>(unseen);
         Collections.sort(resultList);
+
+        // 출력
         System.out.println(resultList.size());
         for (String name : resultList) {
             System.out.println(name);
