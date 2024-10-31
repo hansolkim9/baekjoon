4c49063594e4e25ba6938c31f2315c0c789e40e2
hansolkim9 <masicer2@gmail.com>
2024-10-31 18:12:06 +0900

    11047

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -6,33 +6,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int N = scanner.nextInt(); // 듣도 못한 사람 수
-        int M = scanner.nextInt(); // 보도 못한 사람 수
+        int N = scanner.nextInt(); // 동전 종류
+        int K = scanner.nextInt(); // 목표값
+        int[] coin = new int[N];
 
-        Set<String> unheard = new HashSet<>();
-        Set<String> unseen = new HashSet<>();
-
-        // 듣도 못한 사람을 Set에 추가
         for (int i = 0; i < N; i++) {
-            unheard.add(scanner.next());
+            coin[i] = scanner.nextInt();
         }
+        Arrays.sort(coin);
+        int result = 0;
 
-        // 보도 못한 사람을 Set에 추가
-        for (int j = 0; j < M; j++) {
-            String name = scanner.next();
-            if (unheard.contains(name)) {
-                unseen.add(name);  // 듣도 못한 사람에 포함된 경우에만 unseen에 추가
+        for (int i = N-1; i >= 0; i--) {
+            result += K / coin[i];
+            K %= coin[i];
+
+            if (K == 0) {
+                break;
             }
         }
-
-        // 결과 리스트 정렬
-        List<String> resultList = new ArrayList<>(unseen);
-        Collections.sort(resultList);
-
-        // 출력
-        System.out.println(resultList.size());
-        for (String name : resultList) {
-            System.out.println(name);
-        }
+        System.out.println(result);
     }
 }
