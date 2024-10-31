9b9ea367752e3e0fa9297c35de5fbc963b966c87
hansolkim9 <masicer2@gmail.com>
2024-10-31 17:38:23 +0900

    1620

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -1,41 +1,35 @@
 package com.study;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int[] sequence = new int[n];
-        for (int i = 0; i < n; i++) {
-            sequence[i] = scanner.nextInt();
+        int N = scanner.nextInt(); // 도감에 있는 포켓몬 개수
+        int M = scanner.nextInt(); // 맞춰야 하는 문제 개수
+
+        Map<String, Integer> map = new HashMap<>();
+        String[] array = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            String poketmon = scanner.next();
+            map.put(poketmon, i+1);
+            array[i] = poketmon;
         }
 
-        Stack<Integer> stack = new Stack<>();
-        StringBuilder result = new StringBuilder();
-        int current = 1;
+        for (int i = 0; i < M; i++) {
+            String question = scanner.next();
 
-        for (int i = 0; i < n; i++) {
-            int target = sequence[i];
-
-            // target에 도달할때까지 push
-            while (current <= target) {
-                stack.push(current);
-                result.append("+\n");
-                current++;
-            }
-
-            // stack의 최상단값이 target인지 확인
-            if (stack.peek() == target) {
-                stack.pop();
-                result.append("-\n");
-            } else {
-                System.out.println("NO");
-                return;
+            // question이 숫자인지 확인
+            try {
+                int number = Integer.parseInt(question); // 숫자로 변환 시도
+                System.out.println(array[number - 1]); // 배열에서 이름을 찾음
+            } catch (NumberFormatException e) {
+                System.out.println(map.get(question)); // 해시맵에서 번호를 찾음
             }
         }
-        System.out.println(result.toString());
     }
 }
