9466429b7829d6db69feb546bc658c6792ea92d2
hansolkim9 <masicer2@gmail.com>
2024-11-02 17:04:01 +0900

    1463

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -5,27 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-        int T = scanner.nextInt();
+        int[] dp = new int[n + 1];
+        dp[1] = 0; // 1은 1로 만드는 데 연산이 필요 없음
 
-        int[][] dp = new int[41][2];
-        dp[0][0] = 1;
-        dp[0][1] = 0;
-        dp[1][0] = 0;
-        dp[1][1] = 1;
+        for (int i = 2; i <= n; i++) {
+            // 기본적으로 i에서 1을 뺀 경우
+            dp[i] = dp[i - 1] + 1;
 
-        // 채우기
-        for (int i = 2; i <= 40; i++) {
-            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
-            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+            // i가 2로 나누어떨어질 경우
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            // i가 3으로 나누어떨어질 경우
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
         }
 
-        // 테스트 케이스 입력 및 출력
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < T; i++) {
-            int n = scanner.nextInt();
-            result.append(dp[n][0]).append(" ").append(dp[n][1]).append("\n");
-        }
-        System.out.print(result);
+        System.out.println(dp[n]);
     }
 }
