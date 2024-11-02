e327f81214fb51322d42da7edce5447ba2fc1391
hansolkim9 <masicer2@gmail.com>
2024-11-02 17:11:26 +0900

    2579

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -7,22 +7,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
+        int[] score = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            score[i] = scanner.nextInt();
+        }
+
+        if (n == 1) {
+            System.out.println(score[1]);
+            return;
+        }
+
         int[] dp = new int[n + 1];
-        dp[1] = 0; // 1은 1로 만드는 데 연산이 필요 없음
+        dp[1] = score[1];
+        dp[2] = score[1] + score[2];
 
-        for (int i = 2; i <= n; i++) {
-            // 기본적으로 i에서 1을 뺀 경우
-            dp[i] = dp[i - 1] + 1;
-
-            // i가 2로 나누어떨어질 경우
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            }
-
-            // i가 3으로 나누어떨어질 경우
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2] + score[i], dp[i - 3] + score[i - 1] + score[i]);
         }
 
         System.out.println(dp[n]);
