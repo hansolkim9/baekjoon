203a7f423ef9b5ef1ea10e3bd5380a1dd59d1f7a
hansolkim9 <masicer2@gmail.com>
2024-11-06 16:08:46 +0900

    8320

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -7,18 +7,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-        System.out.print(n + " ");
 
-        while (true) {
-            if (n == 1) {
-                break;
-            } else if (n%2 == 0) {
-                n = n/2;
-                System.out.printf("%d ", n);
-            } else if (n%2 == 1) {
-                n = n * 3 + 1;
-                System.out.printf("%d ", n);
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; i * j <= n; j++) {
+                result++;
             }
         }
+        System.out.println(result);
     }
 }
