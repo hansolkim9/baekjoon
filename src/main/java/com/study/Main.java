344fc675ce369ea37938f85266f7e60f6cfacb5c
hansolkim9 <masicer2@gmail.com>
2024-11-06 15:30:16 +0900

    4589

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -6,10 +6,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int red = scanner.nextInt();
-        int green = scanner.nextInt();
-        int blue = scanner.nextInt();
+        int T = scanner.nextInt();
+        System.out.println("Gnomes:");
 
-        System.out.println(3*red + 4*green + 5*blue);
+        for (int i = 0; i < T; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            if (a>=b && b>c) {
+                System.out.println("Ordered");
+            } else if (a<=b && b<=c) {
+                System.out.println("Ordered");
+            } else {
+                System.out.println("Unordered");
+            }
+        }
     }
 }
