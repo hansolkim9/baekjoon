8c4e2145940a93ed299ccc89c764dbe48a855acb
hansolkim9 <masicer2@gmail.com>
2024-11-06 15:22:19 +0900

    27959

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -6,15 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] standard = {1, 1, 2, 2, 2, 8};
-        int[] input = new int[6];
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
 
-        for (int i = 0; i < 6; i++) {
-            input[i] = scanner.nextInt();
-        }
-
-        for (int i = 0; i < 6; i++) {
-            System.out.print(standard[i]-input[i] + " ");
+        if (100*N >= M) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
         }
     }
 }
